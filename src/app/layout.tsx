ae629b1069d615f4db9041a4571c840f3b1a0a93
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-02-18 20:18:18 -0300

    Format Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Providers } from '@/components/layout';
 import '@/styles/globals.css';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import c from 'clsx';
 import { GeistMono } from 'geist/font/mono';
 import { GeistSans } from 'geist/font/sans';
