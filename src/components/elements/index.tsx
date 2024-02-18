a80aa2f5407e6e3f4b3cabb4e66d978b07fdfed0
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-02-18 18:21:14 -0300

    Create Icon and Custom Link

--- src/components/elements/index.tsx
+++ src/components/elements/index.tsx
@@ -1,2 +1,4 @@
+export { default as CustomLink } from './custom-link';
 export { default as Heading } from './heading';
+export { default as Icon } from './icon';
 export { default as Text } from './text';
