cd9a66a79c0815eeff7eb07e4a99c6baeb36ccfe
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-02-18 17:33:33 -0300

    Add ESLINT and PRETTIER base config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+export default {
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +11,9 @@ const config: Config = {
       },
     },
   },
+  darkMode: 'class',
+  future: {
+    hoverOnlyWhenSupported: true
+  },
   plugins: [],
-};
-export default config;
+} satisfies Config;
