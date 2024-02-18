0b1e34725917b089f86ae727be4f84e155498ff4
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-02-18 20:06:39 -0300

    Create Container

---
+++ src/components/layout/container.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import c from 'clsx';
+import type { ReactNode } from 'react';
+
+interface ContainerProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export default function Container({ children, className }: ContainerProps) {
+  return (
+    <div className={c('flex flex-col justify-center px-6 sm:px-8', className)}>{children}</div>
+  );
+}
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,6 @@
 export { default as AnimateEnter } from './animate-enter';
 export { default as BackHome } from './back-home';
 export { default as Contact } from './contact';
+export { default as Container } from './container';
 export { default as Providers } from './providers';
 export { default as Separator } from './separator';
