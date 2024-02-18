4e8720af3cf75450dd464f6859da53a755880472
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-02-18 18:15:06 -0300

    Configure Layout, Tailwind, Add some Libs and Create Providers

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,42 @@
+import typograhpy from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      screens: {
+        mdx: '920px'
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)']
+      },
+      colors: {
+        primary: '#161616',
+        secondary: '#6F6F6F',
+        'primary-dark': '#EDEDED',
+        'secondary-dark': '#A0A0A0'
+      },
+      spacing: {
+        'page-top': 'var(--page-top)',
+        'page-top-mobile': 'var(--page-top-mobile)',
+        'page-bottom': 'var(--page-bottom)',
+        'page-bottom-mobile': 'var(--page-bottom-mobile)'
+      },
+      keyframes: {
+        in: {
+          '0%': { transform: 'translateY(18px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' }
+        },
+        'in-reverse': {
+          '0%': { transform: 'translateY(-18px)', opacity: '0' },
+          '100%': { transform: 'translateY(0px)', opacity: '1' }
+        }
+      },
+      animation: {
+        in: 'in .6s both',
+        'in-reverse': 'in-reverse .6s both'
       }
     }
   },
@@ -14,5 +44,5 @@ export default {
   future: {
     hoverOnlyWhenSupported: true
   },
-  plugins: []
+  plugins: [typograhpy]
 } satisfies Config;
