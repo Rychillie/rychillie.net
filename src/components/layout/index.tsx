dcb2772d99c0797e26eb45eb2068ba0f858afbaf
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-02-19 13:38:36 -0300

    Create Badge

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,6 @@
 export { default as AnimateEnter } from './animate-enter';
 export { default as BackHome } from './back-home';
+export { default as Badge } from './badge';
 export { default as Contact } from './contact';
 export { default as Container } from './container';
 export { default as Providers } from './providers';
