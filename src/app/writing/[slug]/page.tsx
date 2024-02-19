2ba021b2ce3a190f8ed64208cc0e33b732beacb9
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-02-19 13:45:01 -0300

    Create useClipboard

---
+++ src/app/writing/[slug]/page.tsx
@@ -0,0 +1,93 @@
+/* eslint-disable prefer-const */
+import { CustomLink, Heading, MDX } from '@/components/elements';
+import { CopyButton, Navigation, Separator } from '@/components/layout';
+import { getBlogPosts } from '@/lib/content';
+import '@/styles/prose.css';
+import { format, parseISO } from 'date-fns';
+import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
+interface Params {
+  params: { slug: string };
+}
+
+export async function generateStaticParams() {
+  return getBlogPosts().map(({ slug }) => ({
+    params: { slug }
+  }));
+}
+
+export async function generateMetadata(params: any): Promise<Metadata | undefined> {
+  let post = getBlogPosts().find((post) => post.slug === params.slug);
+  if (!post) {
+    return;
+  }
+
+  let { title, publishedAt: publishedTime, summary: description, image } = post.metadata;
+
+  let ogImage = image ? `https://rychillie.net${image}` : `https://rychillie.net/og?title=${title}`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: 'article',
+      publishedTime,
+      url: `https://rychillie.net/blog/${post.slug}`,
+      images: [
+        {
+          url: ogImage
+        }
+      ]
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [ogImage]
+    }
+  };
+}
+
+export default function Post({ params }: Params) {
+  let allWritings = getBlogPosts();
+  let post = allWritings.find((post) => post.slug === params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <>
+      <header className="flex flex-col justify-between gap-6">
+        <span>
+          <CustomLink
+            href="/writing"
+            ariaLabel="Back to writing page"
+            arrowIcon
+            hideUnderline
+            className="transition-colors hover:text-secondary dark:hover:text-secondary-dark"
+          >
+            Writing
+          </CustomLink>
+        </span>
+        <Heading>{post.metadata.title}</Heading>
+      </header>
+      <div className="mb-3 flex justify-between text-sm">
+        <time
+          dateTime={post.metadata.publishedAt}
+          className="text-secondary dark:text-secondary-dark"
+        >
+          {format(parseISO(post.metadata.publishedAt), 'MMMM dd yyyy')}
+        </time>
+        <CopyButton />
+      </div>
+      <Separator className="my-8" />
+      <MDX source={post.content} />
+      <Separator className="my-8" />
+      <Navigation allItems={allWritings} currentItem={post} route="writing" />
+    </>
+  );
+}
