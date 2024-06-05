84c6cd885e35516d0bed0e06e9b21e2d272cff38
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-06-05 13:36:12 -0300

    Upgrade App

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 
 const nextConfig = {
   reactStrictMode: false,
-  transpilePackages: ['next-mdx-remote']
+  transpilePackages: ['next-mdx-remote'],
+  experimental: {
+    ppr: 'incremental',
+    reactCompiler: true,
+    after: true
+  }
 };
 
 export default nextConfig;
