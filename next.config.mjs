12d9cda74146cff818aa15d267bc6e72e696c205
Rychillie <rychillie@rychillie.net>
2024-10-08 17:39:47 -0300

    refactor: Remove remotePatterns from images configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com'
-      }
-    ]
-  }
+  reactStrictMode: false
 };
 
 export default nextConfig;
