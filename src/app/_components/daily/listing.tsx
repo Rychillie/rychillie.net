457ad1e7ac2f889dcb27d281041bd34ef5458c82
Rychillie <rychillie@rychillie.net>
2024-10-21 23:55:44 -0300

    Refactor DailyListing component to add unique IDs to each writing entry

--- src/app/_components/daily/listing.tsx
+++ src/app/_components/daily/listing.tsx
@@ -16,12 +16,12 @@ export default function DailyListing() {
       .map((writing) => {
         const publishedDate = parseISO(writing.metadata.publishedAt);
         return (
-          <div key={writing.slug} className="group relative">
+          <div key={writing.slug} id={writing.slug} className="group relative">
             <div className="flex flex-col-reverse">
               <h2 className="text-xl font-bold">{writing.metadata.title}</h2>
               <time
                 dateTime={writing.metadata.publishedAt}
-                className="text-xs lg:absolute lg:right-[calc(100%+36px)] lg:top-2 whitespace-nowrap relative z-[99999] text-neutral-600 dark:text-neutral-400 text-left lg:text-right before:content-[''] lg:before:-right-3 before:w-2.5 before:h-0.5 lg:before:left-auto before:-left-4 before:absolute before:top-1/2 before:translate-y-1/2 before:bg-neutral-200 dark:before:bg-neutral-800 font-medium"
+                className="text-xs lg:absolute lg:right-[calc(100%+36px)] lg:top-2 whitespace-nowrap relative z-[99999] text-neutral-600 dark:text-neutral-400 text-left lg:text-right before:content-[''] lg:before:-right-3 before:w-2.5 before:h-0.5 lg:before:left-auto before:-left-4 before:absolute before:top-1/2 before:bg-neutral-200 dark:before:bg-neutral-800 font-medium"
               >
                 {format(publishedDate, 'MMM d, yyyy', { locale: enUS })}
               </time>
