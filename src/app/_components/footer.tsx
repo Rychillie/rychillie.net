35d9084dbdfee7aae01b27700cf336625d498eae
Rychillie <rychillie@rychillie.net>
2025-01-02 10:01:56 -0300

    Update footer component to display the current year dynamically in the copyright notice

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,9 +1,11 @@
 import CustomLink from './custom-link';
 
 export default function Footer() {
+  const fullYear = new Date().getFullYear();
+
   return (
     <div className="w-full pt-2 pb-8 lg:pb-0 flex-col lg:flex-row justify-start lg:justify-between text-neutral-600 dark:text-neutral-400 items-center gap-4 flex">
-      <div className="text-sm">Copyright 2024 · Made by Rychillie</div>
+      <div className="text-sm">Copyright {fullYear} · Made by Rychillie</div>
       <div className="text-xs font-medium">
         <CustomLink
           href="/about#contact"
